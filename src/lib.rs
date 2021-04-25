d7e58369dec1546864ce633db6656b9f4da91551
Alex Norman <alex@x37v.info>
2021-04-25 12:56:37 -0700

    nanosecond delay

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@ use cortex_m::asm::delay as delay_cycles;
 use stm32h7xx_hal::time::{Hertz, MegaHertz};
 
 pub const MILLI: u32 = 1_000;
+pub const MICRO: u32 = 1_000_000;
+pub const NANO: u32 = 1_000_000_000;
+
 pub const AUDIO_FRAME_RATE_HZ: u32 = 1_000;
 pub const AUDIO_BLOCK_SIZE: u16 = 48;
 pub const AUDIO_SAMPLE_RATE: usize = 48_000;
@@ -16,6 +19,7 @@ pub const AUDIO_SAMPLE_HZ: Hertz = Hertz(48_000);
 pub const CLOCK_RATE_HZ: Hertz = Hertz(480_000_000_u32);
 
 pub const MILICYCLES: u32 = CLOCK_RATE_HZ.0 / MILLI;
+pub const MICROCYCLES: u32 = CLOCK_RATE_HZ.0 / MICRO;
 
 pub type FrameTimer = stm32h7xx_hal::timer::Timer<stm32h7xx_hal::stm32::TIM2>;
 
@@ -30,9 +34,20 @@ pub mod system;
 
 pub mod field;
 
-// Delay for ms, note if interrupts are active delay time will extend
+/// Delay for ms, note if interrupts are active delay time will extend
 pub fn delay_ms(ms: u32) {
     delay_cycles(ms * MILICYCLES);
+}
+
+/// Delay for micro sec, note if interrupts are active delay time will extend
+pub fn delay_us(us: u32) {
+    delay_cycles(us * MICROCYCLES);
+}
+
+/// Delay for nano sec, note if interrupts are active delay time will extend
+pub fn delay_ns(ns: u32) {
+    let cycles = CLOCK_RATE_HZ.0.saturating_mul(ns) / MICRO;
+    delay_cycles(if cycles == 0 { 1 } else { cycles });
 }
 
 // pub fn ms_to_cycles(ms: u32) {
