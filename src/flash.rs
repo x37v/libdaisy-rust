07b6a5f9ee9c23863d469f28042fe9d1ccf75264
Alex Norman <alex@x37v.info>
2021-07-09 14:21:27 -0700

    reading flash!

--- src/flash.rs
+++ src/flash.rs
@@ -13,17 +13,44 @@
 //  pin_group[DSY_QSPI_PIN_NCS] = dsy_pin(DSY_GPIOG, 6);
 
 use stm32h7xx_hal::{
-    gpio::{gpiof, gpiog, Analog, OpenDrain, Output},
-    hal::digital::v2::OutputPin,
+    gpio::{gpiof, gpiog, Analog, Speed},
     prelude::*,
     rcc,
     xspi::{Config, QspiMode, QspiWord},
 };
 
-struct Flash {
+pub struct Flash {
     qspi: stm32h7xx_hal::xspi::Qspi<stm32h7xx_hal::stm32::QUADSPI>,
-    ncs: gpiog::PG6<Output<OpenDrain>>,
 }
+
+/*
+ *
+ * 6.1 STATUS REGISTER
+ * Status Register Format and Status Register Bit Definitionsare described in Table 6.1 & Table 6.2.
+ * 0: WIP write in progress(0 == read, 1 == busy)
+ * 1: WEL write enable (1 == enabled)
+ * 2-5: BP block protection (0 indicates not protected [default])
+ * 6: Quad enable, quad output function enable, (1 = enable)
+ * 7: Status register write disable (1 == write protected [0 = default])
+ *
+ * 6.3 READ REGISTER
+ *
+ *
+ * 8.11 SECTOR ERASE OPERATION (SER, D7h/20h)
+ *  * instruction, 3 byte address
+ *  * WEL is reset after
+ * 8.12 BLOCK ERASE OPERATION (BER32K:52h, BER64K:D8h)
+ *  * instruction, 3 byte address
+ *  * WEL is reset after
+ * 8.13 CHIP ERASE OPERATION (CER, C7h/60h)
+ *  * instruction only
+ *  * WEL is reset after
+ * 8.14 WRITE ENABLE OPERATION (WREN, 06h)
+ *  * instruction only
+ *  * sets WEL
+ * 8.16 READ STATUS REGISTER OPERATION (RDSR, 05h)
+ *  * instruction, 1 byte read
+*/
 
 impl Flash {
     /// Initialize the flash quad spi interface
@@ -38,8 +65,10 @@ impl Flash {
         pf10: gpiof::PF10<Analog>,
         pg6: gpiog::PG6<Analog>,
     ) -> Self {
-        let mut ncs = pg6.into_open_drain_output();
-        ncs.set_high().unwrap();
+        //let rcc = unsafe { &*stm32h7xx_hal::stm32::RCC::ptr() };
+        //rcc.ahb3enr.modify(|_, w| w.qspien().set_bit());
+
+        let _ncs = pg6.into_alternate_af10(); //QUADSPI_BK1_NCS
 
         let sck = pf10.into_alternate_af9();
         let io0 = pf8.into_alternate_af10();
@@ -47,48 +76,63 @@ impl Flash {
         let io2 = pf7.into_alternate_af9();
         let io3 = pf6.into_alternate_af9();
 
-        let config = Config::new(133.mhz()).mode(QspiMode::OneBit);
+        let config = Config::new(1.mhz()).mode(QspiMode::OneBit);
         let mut qspi = regs.bank1((sck, io0, io1, io2, io3), config, &clocks, prec);
 
+        /*
+        //read info
+        let mut info: [u8; 3] = [0xFF; 3];
+        while qspi.is_busy().is_err() {}
+        qspi.read(0x9F, &mut info).unwrap();
+
+        //read status
+        let mut status: [u8; 1] = [0xFF];
+        while qspi.is_busy().is_err() {}
+        qspi.read(0x05, &mut status).unwrap();
+
+        let mut funct: [u8; 1] = [0xFF];
+        while qspi.is_busy().is_err() {}
+        qspi.read(0x48, &mut funct).unwrap();
+        */
+
+        /*
+        //enable write
+
         //setup in qspi mode
-        ncs.set_low().unwrap();
         qspi.write_extended(QspiWord::U8(0x35), QspiWord::None, QspiWord::None, &[])
             .unwrap();
-        while qspi.is_busy().is_err() {}
-        ncs.set_high().unwrap();
         qspi.configure_mode(QspiMode::FourBit).unwrap();
+        */
 
-        Flash { qspi, ncs }
-    }
-}
-
-/// Initialize the flash quad spi interface
-pub fn init(
-    regs: stm32h7xx_hal::device::QUADSPI,
-    prec: rcc::rec::Qspi,
-    clocks: &rcc::CoreClocks,
-    pf6: gpiof::PF6<Analog>,
-    pf7: gpiof::PF7<Analog>,
-    pf8: gpiof::PF8<Analog>,
-    pf9: gpiof::PF9<Analog>,
-    pf10: gpiof::PF10<Analog>,
-    pg6: gpiog::PG6<Analog>,
-) -> stm32h7xx_hal::xspi::Qspi<stm32h7xx_hal::stm32::QUADSPI> {
-    let mut cs = pg6.into_open_drain_output();
-
-    let sck = pf10.into_alternate_af9();
-    let io0 = pf8.into_alternate_af10();
-    let io1 = pf9.into_alternate_af10();
-    let io2 = pf7.into_alternate_af9();
-    let io3 = pf6.into_alternate_af9();
-
-    let config = Config::new(133.mhz()).mode(QspiMode::OneBit);
-    let mut qspi = regs.bank1((sck, io0, io1, io2, io3), config, &clocks, prec);
-    cs.set_high().unwrap();
-    qspi.write_extended(QspiWord::U8(0x35), QspiWord::None, QspiWord::None, &[])
+        //read info
+        let mut info: [u8; 3] = [0; 3];
+        while qspi.is_busy().is_err() {}
+        qspi.read_extended(
+            QspiWord::U8(0x9F),
+            QspiWord::None,
+            QspiWord::None,
+            0,
+            &mut info,
+        )
         .unwrap();
-    while qspi.is_busy().is_err() {}
-    cs.set_low().unwrap();
-    qspi.configure_mode(QspiMode::FourBit).unwrap();
-    qspi
+
+        while qspi.is_busy().is_err() {}
+        qspi.write_extended(QspiWord::U8(0x06), QspiWord::None, QspiWord::None, &[])
+            .unwrap();
+
+        //read status
+        let mut status: [u8; 1] = [0xFF];
+        while qspi.is_busy().is_err() {}
+        qspi.read_extended(
+            QspiWord::U8(0x05),
+            QspiWord::None,
+            QspiWord::None,
+            0,
+            &mut status,
+        )
+        .unwrap();
+
+        while qspi.is_busy().is_err() {}
+        Flash { qspi }
+    }
 }
