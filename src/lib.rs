12537b8594166f754104b376a77fb8ccbd84e3f0
Alex Norman <alex@x37v.info>
2022-02-05 19:25:24 -0800

    WIP field driver and deps plus some timing utilities

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,13 @@ use cortex_m::asm::delay as delay_cycles;
 
 use stm32h7xx_hal::time::{Hertz, MegaHertz};
 
+///export hal
+pub use stm32h7xx_hal as hal;
+
 pub const MILLI: u32 = 1_000;
+pub const MICRO: u32 = 1_000_000;
+pub const NANO: u32 = 1_000_000_000;
+
 pub const AUDIO_FRAME_RATE_HZ: u32 = 1_000;
 pub const AUDIO_BLOCK_SIZE: u16 = 48;
 pub const AUDIO_SAMPLE_RATE: usize = 48_000;
@@ -16,6 +22,7 @@ pub const AUDIO_SAMPLE_HZ: Hertz = Hertz(48_000);
 pub const CLOCK_RATE_HZ: Hertz = Hertz(480_000_000_u32);
 
 pub const MILICYCLES: u32 = CLOCK_RATE_HZ.0 / MILLI;
+pub const MICROCYCLES: u32 = CLOCK_RATE_HZ.0 / MICRO;
 
 pub type FrameTimer = stm32h7xx_hal::timer::Timer<stm32h7xx_hal::stm32::TIM2>;
 
@@ -28,9 +35,41 @@ pub mod prelude;
 pub mod sdram;
 pub mod system;
 
-// Delay for ms, note if interrupts are active delay time will extend
-pub fn delay_ms(ms: u32) {
-    delay_cycles(ms * MILICYCLES);
+pub mod field;
+
+/// Delay by milliseconds.
+///
+/// # Arguments
+/// * `t` - the number of milliseconds to delay.
+///
+/// # Remarks
+/// If interrupts are active delay time will extend.
+pub fn delay_ms(t: u32) {
+    delay_cycles(t * MILICYCLES);
+}
+
+/// Delay by microseconds.
+///
+/// # Arguments
+/// * `t` - the number of microseconds to delay.
+///
+/// # Remarks
+/// If interrupts are active delay time will extend.
+pub fn delay_us(t: u32) {
+    delay_cycles(t * MICROCYCLES);
+}
+
+/// Delay by nanoseconds.
+///
+/// # Arguments
+/// * `t` - the number of nanoseconds to delay.
+///
+/// # Remarks
+/// If interrupts are active delay time will extend.
+#[inline]
+pub fn delay_ns(t: u32) {
+    let cycles = CLOCK_RATE_HZ.0.saturating_mul(t) / MICRO;
+    delay_cycles(if cycles == 0 { 1 } else { cycles });
 }
 
 // pub fn ms_to_cycles(ms: u32) {
